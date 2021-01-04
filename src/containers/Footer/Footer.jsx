0e0df485ba7bf42f71691f038cea504e9433645a
David <d.pitarch@outlook.es>
2021-01-04 12:35:22 +0100

    Home view finished for all devices

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 // Importo las dependencias necesarias
 import React from 'react';
+import * as FaIcons from "react-icons/fa";
 
 
 // Importo la hoja de estilos
@@ -11,7 +12,13 @@ const Footer = () => {
 
 
     return (
-        <div className="footer-container">FOOTER</div>
+        <div className="footer-container">
+            <div className="upper-footer">
+                <div id="footer-icons" className="linkedin-icon"><a href="https://www.linkedin.com/in/david-pitarch" target="_blank" rel="noreferrer"><FaIcons.FaLinkedinIn /></a></div>
+                <div id="footer-icons" className="github-icon"><a href="https://github.com/Davids-dir" target="_blank" rel="noreferrer"><FaIcons.FaGithub /></a></div>
+            </div>
+            <div className="lower-footer"><span>Creado por David Pitarch Gonzalez 2021 ©</span></div>
+        </div>
     )
 }
 
