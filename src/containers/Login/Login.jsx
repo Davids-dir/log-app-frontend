39e8bc945032c4495d09693807b55aa9ef0cdd90
David <d.pitarch@outlook.es>
2021-01-04 17:18:44 +0100

    Create Form component to Login view

--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -1,60 +1,24 @@
 // Importo las dependencias necesarias
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import axios from 'axios';
+import React from 'react';
+
+
+// Importo la libreria de MDB React
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 // Importo la hoja de estilos
 import './Login.scss';
+import InputPage from '../../components/Form/Form';
 
 
 // Logica del contenedor LOGIN
 const Login = () => {
 
-    // Hook de estado
-    const [user, setUser] = useState({ email: '', password: '', })
-
-    // Evento donde asigno el valor del Input al 'user'
-    const eventHandler = e => { setUser({ ...user, [e.target.name]: e.target.value }) }
-
-    const redirect = useHistory();
-
-    // Funcion para conseguir la IP del dispositivo del usuario que hace Login
-    const getIP = () => {
-
-        axios.get('https://api.ipify.org?format=json')
-
-            .then(res => localStorage.setItem('user_IP', JSON.stringify(res.data)))
-            .catch(error => { console.log(error) })
-
-    }
-
-    // Funcion para el envio de datos contra la DB
-    const SendData = () => {
-
-        getIP();
-
-        // POST hacia el endpoint de Login
-        axios.post('http://localhost:8000/api/user/login', user)
-
-            .then(res => {
-                localStorage.setItem('user', JSON.stringify(res.data))
-
-                setTimeout (() => {
-                    redirect.push('/user/profile')
-                }, 1500)
-                
-            })
-            .catch(error => console.log(error))
-    }
-
     return (
         <div className="loginContainer">
             <form className="formContainer">
-                <label>Correo electrónico</label>
-                <input type="text" name='email' onChange={eventHandler} />
-                <label>Contraseña</label>
-                <input type="password" name='password' onChange={eventHandler} />
-                <button type="button" onClick={() => SendData()}>Login</button>
+                <InputPage />
             </form>
         </div>
     )
